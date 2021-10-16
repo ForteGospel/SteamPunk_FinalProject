88a43a372a0faad0673242e52471bf01453e2b4c
Jonathan Quiben <54368132+ForteGospel@users.noreply.github.com>
2021-10-17 02:42:49 +0300

    added clouds

---
+++ Assets/Scripts/MaterialOffsetController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialOffsetController : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 speed = new Vector2(1, 0);
+
+    Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = gameObject.GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        material.mainTextureOffset += speed * Time.deltaTime;
+    }
+}
