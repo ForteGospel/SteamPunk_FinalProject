7ddbce9c6671062bd09810a4b276a2006598fe0b
Jonathan Quiben <54368132+ForteGospel@users.noreply.github.com>
2021-10-16 23:22:33 +0300

    scene addons

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 center;
+
+    [SerializeField]
+    float speed;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(center, transform.up, speed * Time.deltaTime);
+    }
+}
